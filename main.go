7de2be080219db2c1d1321815cadc1bacf9df6b1
Aditya Kumar <aditya4u164@gmail.com>
2026-08-02 03:56:30 +0530

    Add TI

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"blackbox/assets"
 	"log"
 	"time"
-
-	"charm.land/bubbles/v2/textarea"
+	"charm.land/bubbles/v2/textinput"
 	tea "charm.land/bubbletea/v2"
 	"charm.land/lipgloss/v2"
 )
 
 var Logo string = assets.GetLogo() 
+
+var (
+	InputStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("#6B61BA"))
+)
 
 type File struct{
 	path string
@@ -21,6 +24,11 @@ type File struct{
 }
 
 func initModel(m *model){
+	ti := textinput.New()
+	ti.Placeholder = `Press "/" to add link.`
+	ti.Prompt = " >  "
+
+	m.AddLink = ti
 	m.isLoading = true
 }
 
@@ -28,13 +36,12 @@ func initModel(m *model){
 type model struct{
 	height int
 	width int
-	AddLink textarea.Model
+	AddLink textinput.Model
 	Files []File
 	isLoading bool
 }
 
 type IOFetchMsg struct{}
-
 func fetchFiles() tea.Msg {
 	time.Sleep(5* time.Second)
 	return IOFetchMsg{}
@@ -45,24 +52,40 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model,tea.Cmd){
+	var cmd tea.Cmd
+
 	switch msg := msg.(type){
 	case tea.WindowSizeMsg:
 		m.height = msg.Height
 		m.width  = msg.Width
+		m.AddLink.SetWidth(min(60,m.width-3))
+		return m, nil
 	case tea.KeyMsg:
 		switch msg.String(){
 		case "q":
 			return m, tea.Quit
+		case "/":
+			cmd = m.AddLink.Focus()
+			return m,cmd
+		case "enter":
+			if m.AddLink.Focused(){
+				m.AddLink.Blur()
+				m.AddLink.SetValue("")
+			}
+		case "esc":
+			if m.AddLink.Focused(){
+				m.AddLink.Blur()
+			}	
 		}
 	}
-	
-	return m,nil
+	m.AddLink, cmd = m.AddLink.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() tea.View{
+	theLogo := lipgloss.Place(m.width,7,lipgloss.Center,lipgloss.Center,Logo)
 
-	v:= tea.NewView(lipgloss.Place(m.width,7,lipgloss.Center,lipgloss.Center,Logo))
-
+	v:= tea.NewView(lipgloss.JoinVertical(lipgloss.Center,theLogo, InputStyle.Render(m.AddLink.View())))
 	v.WindowTitle = "BlackBox"
 	v.BackgroundColor = lipgloss.Color("#282836")
 	v.AltScreen = true
