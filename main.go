540b8d5ef6e46f5ffd6dff1d9bb7aad2e9b03783
Aditya Kumar <aditya4u164@gmail.com>
2026-08-01 10:27:33 +0530

    Add logo and basic TUI

--- main.go
+++ main.go
@@ -1,17 +1,80 @@
 package main
 
 import (
-	"blackbox/downloader"
-	"fmt"
+	"blackbox/assets"
+	"log"
+	"time"
+
+	"charm.land/bubbles/v2/textarea"
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 )
 
-func main() {
-	var url string
+var Logo string = assets.GetLogo() 
 
-	fmt.Printf("Enter URL: ")
-	fmt.Scanln(&url)
-	err := downloader.DownloadFile(url)
-	if err!= nil{
-		fmt.Println(err)
+type File struct{
+	path string
+	name string 
+	progress float32
+	isSelected bool
+	isCompleted bool
+}
+
+func initModel(m *model){
+	m.isLoading = true
+}
+
+
+type model struct{
+	height int
+	width int
+	AddLink textarea.Model
+	Files []File
+	isLoading bool
+}
+
+type IOFetchMsg struct{}
+
+func fetchFiles() tea.Msg {
+	time.Sleep(5* time.Second)
+	return IOFetchMsg{}
+}
+
+func (m model) Init() tea.Cmd { 
+	return fetchFiles
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model,tea.Cmd){
+	switch msg := msg.(type){
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width  = msg.Width
+	case tea.KeyMsg:
+		switch msg.String(){
+		case "q":
+			return m, tea.Quit
+		}
 	}
+	
+	return m,nil
+}
+
+func (m model) View() tea.View{
+
+	v:= tea.NewView(lipgloss.Place(m.width,7,lipgloss.Center,lipgloss.Center,Logo))
+
+	v.WindowTitle = "BlackBox"
+	v.BackgroundColor = lipgloss.Color("#282836")
+	v.AltScreen = true
+	return v
+}
+
+func main() {
+	m := model{}
+	initModel(&m)
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err!=nil{
+		log.Fatal("Error Starting tea program | Crashed...")
+	}
+
 }
