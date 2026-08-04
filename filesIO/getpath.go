8a5c9e0be97ef33dcb0b22b01146e198ac673586
Aditya Kumar <aditya4u164@gmail.com>
2026-08-04 11:43:31 +0530

    support linux and mac

--- filesIO/getpath.go
+++ filesIO/getpath.go
@@ -8,22 +8,41 @@ import (
 
 func GetPath() string {
 	var filePath string
-	osName := runtime.GOOS
-	if osName == "windows"{
+	var dirPath string
+
+
+	switch runtime.GOOS {
+	case "windows":
 		appData := os.Getenv("LOCALAPPDATA")
-		dirPath := filepath.Join(appData,"blackbox")
-		filePath = filepath.Join(dirPath,"files.csv")
-		_, err := os.Stat(filePath)
-		if err!=nil{
-			err := os.MkdirAll(dirPath,0755)
-			if err!=nil{
-				return ""
-			}
-			
-			_, err = os.Create(filePath)
-			if err!=nil{
-				return ""
-			}
+		dirPath = filepath.Join(appData, "blackbox")
+	case "linux":
+		configDir, err := os.UserConfigDir()
+		if err != nil {
+			return ""
+		}
+		dirPath = filepath.Join(configDir, "blackbox")
+	case "darwin":
+		configDir, err := os.UserConfigDir()
+		if err != nil {
+			return ""
+		}
+		dirPath = filepath.Join(configDir, "blackbox")
+
+	default:
+		return ""
+	}
+
+	err := os.MkdirAll(dirPath, 0755)
+	if err != nil {
+		return ""
+	}
+
+	filePath = filepath.Join(dirPath, "files.csv")
+	_, err = os.Stat(filePath)
+	if err != nil {
+		_, err = os.Create(filePath)
+		if err != nil {
+			return ""
 		}
 	}
 	return filePath
