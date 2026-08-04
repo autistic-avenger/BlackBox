95f0049569d7dde080646d238cb6f029b81445bb
Aditya Kumar <aditya4u164@gmail.com>
2026-08-04 12:53:40 +0530

    remove useless code

--- downloader/helpers/downloadPath.go
+++ downloader/helpers/downloadPath.go
@@ -3,17 +3,12 @@ package helpers
 import (
 	"os"
 	"path/filepath"
-	"runtime"
 )
 
 func GetDownloadPath() string {
-	osName := runtime.GOOS
-	if osName == "windows"{
-		homeDir,err := os.UserHomeDir()
-		if err!=nil{
-			return ""
-		}
-		return filepath.Join(homeDir,"Downloads")
+	homeDir,err := os.UserHomeDir()
+	if err!=nil{
+		return ""
 	}
-	return ""
+	return filepath.Join(homeDir,"Downloads")
 }
